4eb8390e4035c274237779787861b8bb1966a70e
tsr07843 <tyler.senter@kub.org>
2023-07-21 12:39:39 -0700

    feat: Animate collapse transitions (Exercise #6)

--- app/components/album-card.js
+++ app/components/album-card.js
@@ -1,8 +1,18 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
+import move from 'ember-animated/motions/move';
+import scale from 'ember-animated/motions/scale';
 
 import './album-card.css';
 
 export default class AlbumCardComponent extends Component {
   @service player;
+
+  *transition({ keptSprites, receivedSprites }) {
+    for (const sprite of [...receivedSprites, ...keptSprites]) {
+      move(sprite);
+      scale(sprite);
+    }
+    yield;
+  }
 }
