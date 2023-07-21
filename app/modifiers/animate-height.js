d99ee354e2ef78b82e17d65694a1645cacd98102
tsr07843 <tyler.senter@kub.org>
2023-07-21 11:56:27 -0700

    feat: Animate collapse transitions (Exercise #4)

--- app/modifiers/animate-height.js
+++ app/modifiers/animate-height.js
@@ -1,9 +1,19 @@
 import { modifier } from 'ember-modifier';
+import { next } from '@ember/runloop';
 
 export default modifier(function animateHeight(
   element,
   positional,
   { isOpen = false }
 ) {
-  // TODO: implement solution for exercise #4
+  if (!isOpen) {
+    element.style.height = '0px';
+    return;
+  }
+  element.style.height = 'auto';
+  const height = element.getBoundingClientRect().height;
+  element.style.height = '0px';
+  next(() => {
+    element.style.height = `${height}px`;
+  });
 });
