d62395e642418e6af81ed156b4e67932757d6bc4
tsr07843 <tyler.senter@kub.org>
2023-07-21 12:01:47 -0700

    refactor: Simplify collapse height calculation

--- app/modifiers/animate-height.js
+++ app/modifiers/animate-height.js
@@ -1,19 +1,10 @@
 import { modifier } from 'ember-modifier';
-import { next } from '@ember/runloop';
 
 export default modifier(function animateHeight(
   element,
   positional,
   { isOpen = false }
 ) {
-  if (!isOpen) {
-    element.style.height = '0px';
-    return;
-  }
-  element.style.height = 'auto';
-  const height = element.getBoundingClientRect().height;
-  element.style.height = '0px';
-  next(() => {
-    element.style.height = `${height}px`;
-  });
+  const targetHeight = isOpen ? `${element.scrollHeight}px` : '0px';
+  element.style.height = targetHeight;
 });
